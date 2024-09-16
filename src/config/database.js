d1f596e851d4de262da897fffa52cfe7f9fdd42d
Chachu21 <tsecha6@gmail.com>
2024-09-16 14:57:00 +0300

    change is done

--- src/config/database.js
+++ src/config/database.js
@@ -5,11 +5,19 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    const uri = process.env.MONGO_URL;
+    if (!uri) {
+      throw new Error("MONGO_URL is not defined");
+    }
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
     process.exit(1);
   }
 };
+
 export default connectDB;
